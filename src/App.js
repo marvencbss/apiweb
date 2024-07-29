c930fbcf59fc0a45339ca463e6bd908dc6d16de2
marvencbss <marvencbs12@gmail.com>
2024-07-29 20:13:07 -0300

    .

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import Header from './Header';
 
 function App() {
   return (
     <div className="App">
+      {/*importar header*/}
+      <Header/>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
---
+++ src/Header.js
@@ -0,0 +1,9 @@
+function Header() {
+    return (
+        <Header>
+            <h1>Componente Header</h1>
+        </Header>
+    );
+}
+
+export default Header;
